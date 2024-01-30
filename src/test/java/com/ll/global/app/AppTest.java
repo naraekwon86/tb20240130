ff16d6cec6413000ab96808a3dd6569ae78aa087
naraekwon86 <wde0530@gmail.com>
2024-01-30 17:07:15 +0900

    start

---
+++ src/test/java/com/ll/global/app/AppTest.java
@@ -0,0 +1,39 @@
+package com.ll.global.app;
+
+import com.ll.standard.util.TestUtil;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import java.io.ByteArrayOutputStream;
+import java.util.Scanner;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class AppTest {
+    @Test
+    @DisplayName("프로그램 시작시 \"== 명언 앱 ==\" 출력")
+    void t1(){
+        ByteArrayOutputStream byteArrayOutputStream = TestUtil.setOutToByteArray();
+
+        new App().run();
+
+        String out = byteArrayOutputStream.toString().trim();
+        TestUtil.clearSetOutToByteArray(byteArrayOutputStream);
+
+        assertThat(out)
+                .contains("== 명언 앱 ==");
+    }
+    @Test
+    @DisplayName("종료")
+    void t2(){
+        Scanner scanner = TestUtil.genScanner("""
+                종료
+                """.stripIndent());
+        ByteArrayOutputStream byteArrayOutputStream = TestUtil.setOutToByteArray();
+
+        String out = byteArrayOutputStream.toString().trim();
+        TestUtil.clearSetOutToByteArray(byteArrayOutputStream);
+
+    }
+
+}
