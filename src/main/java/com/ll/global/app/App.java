ce5d64009b557a12052ff4a2a22702ddd2bca727
naraekwon86 <wde0530@gmail.com>
2024-01-31 14:30:54 +0900

    t7

--- src/main/java/com/ll/global/app/App.java
+++ src/main/java/com/ll/global/app/App.java
@@ -1,5 +1,9 @@
 package com.ll.global.app;
 
+import com.ll.domain.quotation.quotation.entity.Quotation;
+
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Scanner;
 
 public class App {
@@ -12,17 +16,47 @@ public class App {
     public void run() {
         System.out.println("== 명언 앱 ==");
 
-        System.out.print("명언 : ");
-        System.out.print("작가 : ");
+        final List<Quotation> quotations = new ArrayList<>();
 
-        System.out.println("1번 명언이 등록되었습니다.");
+        long lastQuotationId = 0;
+
         while (true) {
-            System.out.print("명령) ");
-            String cmd = scanner.nextLine();
+            final String cmd = scanner.nextLine().trim();
 
-            if (cmd.equals("종료")) break;
+            switch (cmd){
+                case "등록" -> {
+                    System.out.print("명언 : ");
+                    final String content = scanner.nextLine().trim();
+                    System.out.print("작가 : ");
+                    final String authorName = scanner.nextLine().trim();
+
+                    final long id = ++lastQuotationId;
+
+                    Quotation quotation = new Quotation(id, authorName, content);
+                    quotations.add(quotation);
+
+                    System.out.println("%d번 명언이 등록되었습니다.".formatted(id));
+                }
+                case "목록" ->{
+                    System.out.println("번호 / 작가 / 명언");
+                    System.out.println("------------------------------");
+
+                    quotations
+                            .reversed()
+                            .forEach(
+                                    quotation -> System.out.println(
+                                            "%d / %s / %s".formatted(
+                                                    quotation.getId(),
+                                                    quotation.getAuthorName(),
+                                                    quotation.getContent()
+                                            )
+                                    )
+                            );
+                }
+                case "종료" ->{
+                    return;
+                }
+            }
         }
-
-        System.out.println("종료");
     }
 }
--- src/test/java/com/ll/global/app/AppTest.java
+++ src/test/java/com/ll/global/app/AppTest.java
@@ -11,7 +11,7 @@ import static org.assertj.core.api.Assertions.assertThat;
 
 public class AppTest {
     private String run(final String cmd){
-        final Scanner scanner = TestUtil.genScanner(cmd.stripIndent().trim());
+        final Scanner scanner = TestUtil.genScanner(cmd.stripIndent().trim() + "\n종료");
 
         final ByteArrayOutputStream byteArrayOutputStream = TestUtil.setOutToByteArray();
 
@@ -24,9 +24,7 @@ public class AppTest {
     @Test
     @DisplayName("프로그램 시작시 \"== 명언 앱 ==\" 출력")
     void t1(){
-        final String out = run("""
-                종료
-                """);
+        final String out = run("");
 
         assertThat(out)
                 .contains("== 명언 앱 ==");
@@ -35,9 +33,7 @@ public class AppTest {
     @Test
     @DisplayName("종료")
     void t2(){
-        final String out = run("""
-                종료
-                """);
+        final String out = run("");
     }
     @Test
     @DisplayName("등록")
@@ -46,7 +42,6 @@ public class AppTest {
                 등록
                 현재를 사랑하라.
                 작자미상
-                종료
                 """);
 
         assertThat(out)
@@ -55,5 +50,72 @@ public class AppTest {
                 .contains("1번 명언이 등록되었습니다.");
 
     }
+    @Test
+    @DisplayName("등록할 때 마다 번호가 1씩 증가, 1건 등록")
+    void t4(){
+        final String out = run("""
+                등록
+                현재를 사랑하라.
+                작자미상
+                """);
 
+        assertThat(out)
+                .contains("1번 명언이 등록되었습니다.")
+                .doesNotContain("2번 명언이 등록되었습니다.");
+    }
+    @Test
+    @DisplayName("등록할 때 마다 번호가 1씩 증가, 2건 등록")
+    void t5(){
+        final String out = run("""
+                등록
+                현재를 사랑하라.
+                작자미상
+                등록
+                현재를 사랑하라.
+                작자미상
+                """);
+
+        assertThat(out)
+                .contains("1번 명언이 등록되었습니다.")
+                .contains("2번 명언이 등록되었습니다.")
+                .doesNotContain("3번 명언이 등록되었습니다.");
+    }
+    @Test
+    @DisplayName("목록")
+    void t6(){
+        final String out = run("""
+                등록
+                현재를 사랑하라.
+                작자미상
+                등록
+                과거에 집착하지 마라.
+                작자미상
+                목록
+                """);
+
+        assertThat(out)
+                .contains("번호 / 작가 / 명언")
+                .contains("------------------------------")
+                .contains("2 / 작자미상 / 과거에 집착하지 마라.")
+                .contains("1 / 작자미상 / 현재를 사랑하라.");
+    }
+    @Test
+    @DisplayName("목록 2")
+    void t7(){
+        final String out = run("""
+                등록
+                현재를 사랑하라.
+                작자미상
+                등록
+                과거에 집착하지 마라.
+                홍길동
+                목록
+                """);
+
+        assertThat(out)
+                .contains("번호 / 작가 / 명언")
+                .contains("------------------------------")
+                .contains("2 / 홍길동 / 과거에 집착하지 마라.")
+                .contains("1 / 작자미상 / 현재를 사랑하라.");
+    }
 }
