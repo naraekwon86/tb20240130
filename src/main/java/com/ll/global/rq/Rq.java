e777f7aba139fa7a81035bdb37143d53a85fcbcc
naraekwon86 <wde0530@gmail.com>
2024-01-31 16:47:07 +0900

    rqtest.t5

---
+++ src/main/java/com/ll/global/rq/Rq.java
@@ -0,0 +1,35 @@
+package com.ll.global.rq;
+
+import java.util.Arrays;
+import java.util.Map;
+import java.util.stream.Collectors;
+
+public class Rq {
+    private final String action;
+    private final String queryString;
+    private Map<String, String> params;
+
+    public  Rq(final String cmd) {
+        final String[] cmdBits = cmd.split("\\?", 2);
+        action = cmdBits[0].trim();
+        queryString = cmdBits.length == 2 ? cmdBits[1].trim() : "";
+
+        params = Arrays.stream(queryString.split("&"))
+                .filter(param -> param.contains("="))
+                .map(param -> param.split("=", 2))
+                .collect(Collectors.toMap(
+                        paramBits -> paramBits[0].trim(),
+                        paramBits -> paramBits[1].trim(),
+                        (existing, replacement) -> replacement));
+    }
+    public String getAction() {
+        return action;
+    }
+
+    public String getParameter(final String paramName) {
+       return getParameter(paramName,null);
+    }
+    public String getParameter(final String paramName, final String defaultValue){
+        return params.getOrDefault(paramName,defaultValue);
+    }
+}
---
+++ src/test/java/com/ll/global/rq/RqTest.java
@@ -0,0 +1,52 @@
+package com.ll.global.rq;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class RqTest {
+    @Test
+    @DisplayName("getAction")
+    void t1(){
+        final Rq rq = new Rq("삭제?id=1");
+
+        assertThat(rq.getAction()).isEqualTo("삭제");
+
+    }
+    @Test
+    @DisplayName("getParameter")
+    void t2(){
+        final Rq rq = new Rq("삭제?이름=Paul");
+
+        assertThat(rq.getParameter("이름")).isEqualTo("Paul");
+
+    }
+    @Test
+    @DisplayName("getParameter 2")
+    void t3(){
+        final Rq rq = new Rq("삭제?이름=Alice");
+
+        assertThat(rq.getParameter("이름")).isEqualTo("Alice");
+
+    }
+    @Test
+    @DisplayName("getParameter 3")
+    void t4(){
+        final Rq rq = new Rq("삭제?id=4&이름=Alice");
+
+        assertThat(rq.getParameter("id")).isEqualTo("4");
+        assertThat(rq.getParameter("이름")).isEqualTo("Alice");
+
+    }
+    @Test
+    @DisplayName("getParameterOr")
+    void t5(){
+        final Rq rq = new Rq("삭제?id=4&이름=Alice");
+
+        String age = rq.getParameter("나이","100");
+        assertThat(age).isEqualTo("100");
+
+    }
+
+
+}
