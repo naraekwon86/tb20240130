0c1b839c75a0d60ca66f8ded5390642ec9cf78be
naraekwon86 <wde0530@gmail.com>
2024-02-01 21:49:49 +0900

    mvc 11

---
+++ src/main/java/com/ll/domain/quotation/quotation/service/QuotationService.java
@@ -0,0 +1,37 @@
+package com.ll.domain.quotation.quotation.service;
+
+import com.ll.domain.quotation.quotation.entity.Quotation;
+import com.ll.domain.quotation.quotation.repository.QuotationRepository;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
+
+public class QuotationService {
+    private final QuotationRepository quotationRepository;
+    public QuotationService(){
+      quotationRepository = new QuotationRepository();
+    }
+    public List<Quotation> findAll(){
+        return quotationRepository.findAll();
+    }
+    public void remove(Quotation quotation){
+        quotationRepository.delete(quotation);
+    }
+    public Optional<Quotation> findById(long id){
+        return quotationRepository.findById(id);
+    }
+    public void modify(Quotation quotation, String authorName, String content){
+        quotation.setAuthorName(authorName);
+        quotation.setContent(content);
+
+        quotationRepository.save(quotation);
+    }
+    public Quotation write(String authorName, String content){
+        final Quotation quotation = new Quotation(authorName, content);
+        quotationRepository.save(quotation);
+        return quotation;
+    }
+
+
+}
